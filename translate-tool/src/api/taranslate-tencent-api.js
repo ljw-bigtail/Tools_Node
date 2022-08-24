93bb1d1fcfca12cb05a0c25f3865c5815ccee10e
Leo <leo.lu@orderplus.com>
2022-08-24 17:38:46 +0800

    优化 新的srt文件 自动创建outfile; 删除 旧的key; 添加 腾讯翻译;

--- translate-tool/src/api/taranslate-tencent-api.js
+++ translate-tool/src/api/taranslate-tencent-api.js
@@ -1,34 +1,24 @@
-const crypto = require("crypto");
-const axios = require("axios");
+const tencentcloud = require("tencentcloud-sdk-nodejs");
 const LogProgressBar = require("../tool/log-progress-bar");
 const Cache = require("../tool/cache");
 const pb = new LogProgressBar("翻译进度");
+const TmtClient = tencentcloud.tmt.v20180321.Client;
 
 const config = require("../../api-key");
-const { api, appid, key, waitTime } = config.baidu;
+const { secretId, secretKey, ProjectId, waitTime } = config.tencent;
 
-function getRadom(len) {
-  var chart = "1234567890",
-    value = "";
-  for (let i = 0; i < len; i++) {
-    value += chart.charAt(Math.floor(Math.random() * chart.length));
-  }
-  return value;
-}
-
-function hash(method, s, format) {
-  var sum = crypto.createHash(method);
-  var isBuff = Buffer.isBuffer(s);
-  if (!isBuff && typeof s == "object") {
-    s = JSON.stringify(s);
-  }
-  sum.update(s, isBuff ? "binary" : "utf8");
-  return sum.digest(format || "hex");
-}
-
-function md5(s, format) {
-  return hash("md5", s, format);
-}
+const client = new TmtClient({
+  credential: {
+    secretId,
+    secretKey,
+  },
+  region: "ap-chongqing",
+  profile: {
+    httpProfile: {
+      endpoint: "tmt.tencentcloudapi.com",
+    },
+  },
+});
 
 // 请求队列
 const generate = (p) => {
@@ -97,29 +87,20 @@ async function waitForOnce(q) {
 }
 
 async function getOnce(q) {
-  const salt = getRadom(8);
-  const params = {
-    from: "en",
-    to: "zh",
-    q,
-    appid,
-    salt,
-    sign: md5(`${appid}${q}${salt}${key}`),
-  };
-  const ajax = await axios.get(api, {
-    params,
-    timeout: 10000,
+  const ajax = await client.TextTranslate({
+    SourceText: q,
+    Source: "en",
+    Target: "zh",
+    ProjectId,
   });
-  if (ajax.data.error_code) {
+  if (ajax.Error) {
     // TODO 断点续传
     // 先保存 然后分析数据 从当前位置再次执行
     // throw new Error(`--------\n接口异常：${ajax.data.error_code}; \n查询字段：${q}\n--------`)
-    console.log(
-      `--------\n接口异常：${ajax.data.error_code}; \n查询字段：${q}`
-    );
+    console.log(`--------\n接口异常：${ajax.Error.Message}; \n查询字段：${q}`);
     return null;
   }
-  return ajax.data.trans_result[0].dst;
+  return ajax.TargetText;
 }
 
 module.exports = translateArr;
--- translate-tool/srt.js
+++ translate-tool/srt.js
@@ -1,34 +1,49 @@
 const fs = require("fs")
-const translateArr = require("./src/api/taranslate-baidu-api")
+// const translateArr = require("./src/api/taranslate-baidu-api")
+const translateArr = require("./src/api/taranslate-tencent-api")
 const Cache = require('./src/tool/cache')
 
-const fileName = 'Build and Deploy a Modern Web 3.0 Blockchain App | Solidity, Smart Contracts, Crypto.srt'
+const fileName = 'JavaScript Fighting Game Tutorial with HTML Canvas.srt'
 
 fs.readFile('./file/' + fileName, 'utf-8', function (err, str) {
   if (err) {
     console.log(err, 'err');
-  } else {
-    let arr = srtToJson(str)
-    var pauseIndex = Cache.get().index
-    if(typeof pauseIndex == 'number'){
-      arr = arr.slice(pauseIndex, -1)
-    }
-    translateArr(arr.map(item => item[2]))
-      .then(res => mixLangs(res, arr))
-      .then(res => readCache(res, pauseIndex))
-      .then(res => arrToSRT(res))
-      .then(res => {
-        fs.writeFile('./end/' + fileName, res, err => {
-          if (err) {
-            console.log(err, 'writeFile error');
-            return
-          }
-          console.log('\nwriteFile success');
-        })
-      })
-      .catch(err => console.log(err, 'err'))
+    return
+  }
+  try {
+    fs.readFileSync('./end/' + fileName, 'utf-8')
+    run(str)
+  } catch (error) {
+    fs.writeFile('./end/' + fileName, '', function(){
+      run(str)
+    });
   }
 })
+
+// api test
+// translateArr(["test", 'word'])
+
+function run(str){
+  let arr = srtToJson(str)
+  var pauseIndex = Cache.get().index
+  if(typeof pauseIndex == 'number'){
+    arr = arr.slice(pauseIndex, -1)
+  }
+  translateArr(arr.map(item => item[2]))
+    .then(res => mixLangs(res, arr))
+    .then(res => readCache(res, pauseIndex))
+    .then(res => arrToSRT(res))
+    .then(res => {
+      fs.writeFile('./end/' + fileName, res, err => {
+        if (err) {
+          console.log(err, 'writeFile error');
+          return
+        }
+        console.log('\nwriteFile success');
+      })
+    })
+    .catch(err => console.log(err, 'err'))
+}
 
 function readCache(res, pauseIndex){
   if(typeof pauseIndex == 'number'){
@@ -48,7 +63,6 @@ function srtToJson(str){
 }
 
 function arrToSRT(arr) {
-  console.log(arr);
   return arr.map(item => {
     return item.join('\n')
   }).join('\n\n')
